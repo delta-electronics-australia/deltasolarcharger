7482bb07e31eff89bcfb2581e6e0fbc6052ca4bd
Ben Ong <jgv115@gmail.com>
2019-03-14 10:34:59 +1100

    added dependencies and start.sh script

---
+++ start.sh
@@ -0,0 +1,3 @@
+lxterminal --working-directory=/home/pi/deltasolarcharger/deltasolarcharger -e sudo python3 deltasolarchargerlauncher.py &
+lxterminal --working-directory=/home/pi/deltasolarcharger/deltasolarcharger/ocppserver -e sudo python3 ocppserver.py
+
