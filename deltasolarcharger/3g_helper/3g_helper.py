7bd4ff9964aebacc542772d4f401d7835e679cd2
jgv115 <jgv115@gmail.com>
2019-06-18 23:18:26 +1000

    Add a 3g helper that will run in the background

---
+++ deltasolarcharger/3g_helper/3g_helper.py
@@ -0,0 +1,45 @@
+import sys
+import datetime
+import time
+import os
+import select
+import pprint
+from systemd import journal
+
+# Create a systemd.journal.Reader instance
+j = journal.Reader()
+
+# Set the reader's default log level
+j.log_level(journal.LOG_INFO)
+
+# Only include entries since the current box has booted.
+j.this_boot()
+j.this_machine()
+
+# Filter log entries
+j.add_match(_SYSTEMD_UNIT='NetworkManager.service')
+
+# Move to the end of the journal
+j.seek_tail()
+
+# Important! - Discard old journal entries
+j.get_previous()
+
+# Create a poll object for journal entries
+p = select.poll()
+
+# Register the journal's file descriptor with the polling object.
+journal_fd = j.fileno()
+poll_event_mask = j.get_events()
+p.register(journal_fd, poll_event_mask)
+
+# Poll for new journal entries every 250ms
+while True:
+    if p.poll(150):
+        if j.process() == journal.APPEND:
+            for entry in j:
+                # pprint.pprint(entry)
+                if "ClientIdsExhausted" in entry["MESSAGE"]:
+                    os.system(
+                        'sudo qmicli -d /dev/cdc-wdm0 --wds-get-packet-service-status --device-open-sync -p; reboot')
+                print('*********************************')
