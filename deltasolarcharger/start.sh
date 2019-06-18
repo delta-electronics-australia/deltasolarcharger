170dbc71ebaf33a73c6d9c774d97b64437ef7675
jgv115 <jgv115@gmail.com>
2019-06-18 23:19:23 +1000

    Add 3g helper to the start script

--- deltasolarcharger/start.sh
+++ deltasolarcharger/start.sh
@@ -1,3 +1,3 @@
 lxterminal --working-directory=/home/pi/deltasolarcharger/deltasolarcharger -e sudo python3 deltasolarchargerlauncher.py &
 lxterminal --working-directory=/home/pi/deltasolarcharger/deltasolarcharger/ocppserver -e sudo python3 ocppserver.py
-
+lxterminal --working-directory=/home/pi/deltasolarcharger/deltasolarcharger/3g_helper -e sudo python3 3g_helper.py
